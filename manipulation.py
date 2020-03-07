3067dc5fb573de30e65500665b470a4a4fa9b069
DragomirRhv <dragomir.ryahov@gmail.com>
2020-03-07 22:20:09 +0200

    invert function added

--- manipulation.py
+++ manipulation.py
@@ -24,9 +24,9 @@ def rotate_left(image):
     changed_items = []
     for i in range(0, len(image) - 1):
         for j in range(0, len(image) - 1):
-            if image[i][n-j] not in changed_items:
+            if image[i][n - j] not in changed_items:
                 image[i][n - j], image[n - j][i] = image[n - j][i], image[i][n - j]
-                changed_items.append(image[n-j][i])
+                changed_items.append(image[n - j][i])
 
     for i in range(0, len(image) // 2):
         for j in range(0, len(image)):
@@ -34,4 +34,25 @@ def rotate_left(image):
 
     return image
 
+
+d = [[(155, 100, 15), (10, 10, 10), (100, 100, 90)], [(200, 15, 115), (140, 130, 120), (50, 60, 70)],
+     [(100, 155, 15), (30, 120, 150), (45, 15, 90)]]
+h = [[(100, 155, 15)]]
+
+
+def invert(image):
+    inverted_image = []
+    i = 0
+    for row in image:
+        for r, g, b in row:
+            current_pixel = (255 - r, 255 - g, 255 - b)
+            if len(inverted_image) <= i:
+                inverted_image.append([current_pixel])
+            else:
+                inverted_image[i].append(current_pixel)
+        i += 1
+    return inverted_image
+
+
+print(invert(d))
 
